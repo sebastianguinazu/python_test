fe210997a7f1241edb6e73f75ec1dc6a6b5fd0ba
sebastianguinazu <sebastianguinazu@gmail.com>
2021-03-11 10:59:23 -0300

    Prueba de Python

---
+++ data_prueba.py
@@ -0,0 +1,19 @@
+def load_data():
+  import pandas as pd
+  import numpy as np
+  import warnings
+  import pandas as pd
+  from pandas.core.common import SettingWithCopyWarning
+
+  warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+  
+  from sklearn.datasets import load_wine
+
+  wine = load_wine()
+  df = pd.DataFrame(data=wine['data'], columns = wine['feature_names'])
+  df['target'] = wine['target']
+
+  for var in ['alcohol', 'magnesium', 'proline']:
+    df[var][np.random.choice([True, False], size=df.shape[0], p=[.2,.8])] = np.NaN
+  
+  return df
