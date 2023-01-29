79448bd4b17927531d6cc8097f814292ccfa8f1b
Sebastián Guiñazú <sebastianguinazu@gmail.com>
2023-01-29 16:26:48 -0300

    Update data_prueba.py

--- data_prueba.py
+++ data_prueba.py
@@ -18,9 +18,13 @@ def load_data():
   df.loc[df['color_intensity']<=4, 'color_intensity_cat'] = 'little intense'
   df.loc[((df['color_intensity']>4) & (df['color_intensity']<=8)), 'color_intensity_cat'] = 'medium intensity'
   df.loc[df['color_intensity']>8, 'color_intensity_cat'] = 'very intense'
-  df.drop('color_intensity', axis=1)  
+  df = df.drop('color_intensity', axis=1)  
     
   df.loc[(df.target == 1),'target'] = 0
   df.loc[(df.target == 2),'target'] = 1
+  
+  idx_tochange = rng.choice(np.argwhere(list(df['target'] == 0)).ravel()
+                        ,size=10,replace=False)
+  df.loc[idx_tochange, 'target'] = 1
   
   return df
